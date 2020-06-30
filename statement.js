d67abaf19743bd139607d453508a2e4cc9a27010
RomanPilguy <roman-lubino@yandex.ru>
2020-06-30 06:48:52 +0300

    v 1.2

--- statement.js
+++ statement.js
@@ -1,6 +1,9 @@
-const InvoiceCreator = require('./invoiceCreator')
-const ComedyStrategy = require('./comedyInvoice')
-const TragedyStrategy = require('./tragedyInvoice')
+const InvoiceCreator = require('./invoiceCreator.js')
+const ComedyStrategy = require('./comedyInvoice.js')
+const TragedyStrategy = require('./tragedyInvoice.js')
+
+const plays = require('./plays.json') // Возможно plays и invoices должны быть разными таблицами, связанными по ключу playId
+
 const invoiceCreator = new InvoiceCreator()
 invoiceCreator.use(new ComedyStrategy())
 invoiceCreator.use(new TragedyStrategy())
@@ -9,12 +12,14 @@ function statement(invoice) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Счет для ${invoice.customer}\n`;
+    
     let comedyCount = 0
     const format = new Intl.NumberFormat("ru-RU",
                         { style: "currency", currency: "RUB",
                           minimumFractionDigits: 2 }).format;
    
     for (let perf of invoice.performances) {
+        let play = plays[perf.playId] || {name : "Неизвестное произведение"}
         if (perf.type === 'comedy') {
             comedyCount ++
         }
@@ -28,7 +33,7 @@ function statement(invoice) {
         }
 
     // Вывод строки счета
-        result += ` ${perf.playId}: ${format(cost / 100)}`;
+        result += ` ${play.name}: ${format(cost / 100)}`;
         result += ` (${perf.audience} мест)\n`;
         
     }
--- test.js
+++ test.js
@@ -1,5 +1,27 @@
 const statement = require('./statement.js')
+const ComedyInvoiceStrategy = require('./comedyInvoice')
+const TragedyInvoiceStrategy = require('./tragedyInvoice')
 const  invoices  = require('./invoices.json')
+
+const comedyStrategy = new ComedyInvoiceStrategy()
+const comedyPerfomance = {
+    "playId": "Отелло",
+    "audience": 40,
+    "type": "comedy"
+    }
+console.log(comedyStrategy.createInvoice(comedyPerfomance))
+// { cost: 62000, credits: 10 }
+
+const tragedyStrategy = new TragedyInvoiceStrategy()
+const tragedyPerfomance = {
+    "playId": "Гамлет",
+    "audience": 55,
+    "type": "tragedy"
+    }
+console.log(tragedyStrategy.createInvoice(tragedyPerfomance))
+//{ cost: 65000, credits: 25 }
+
+
 
 
 console.log(statement(invoices[0]))
